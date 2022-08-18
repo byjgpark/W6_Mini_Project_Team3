3bcef2be25386de2afc45e4adb3f906f64211c2d
PARK-JAE-JEONG <109066569+PARK-JAE-JEONG@users.noreply.github.com>
2022-08-18 17:53:26 +0900

    Update ListPage.jsx

--- src/pages/list/ListPage.jsx
+++ src/pages/list/ListPage.jsx
@@ -13,24 +13,20 @@ const ListPage = () => {
   const postList = useSelector(state => state.list.postList)
   const param = useParams()
 
-
   useEffect(() => {
     dispatch(_getPost());
   }, []);
 
 
-
   return (
     <>
-      <StListContainer>
-        <StButtonContainer>
-          <Header />
-        </StButtonContainer>
+    <StListContainer>
+      <StButtonContainer>
+        <Header />
+      </StButtonContainer>
         <StList>
           {postList?.map((post) => {
-
             if(post.ages === param.id){
-
               return (
                 <Card
                   key={post.id}
@@ -41,20 +37,27 @@ const ListPage = () => {
                   body={post.content}
                   star={post.star}
                 />
-              );
-            } else {
-              return null;
-            }
+              )}else if(param.id === undefined) {
+                return (
+                  <Card
+                    key={post.id}
+                    id={post.id}
+                    image={post.imgUrl}
+                    title={post.title}
+                    place={post.place}
+                    body={post.content}
+                    star={post.star}
+                  />
+                )
+              }else{
+                return(
+                  null
+                )
+              }
           })}
         </StList>
-        <TopBtn
-          onClick={() =>
-            window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
-          }
-        >
-          TOP
-        </TopBtn>
-      </StListContainer>
+        <TopBtn onClick={() => window.scrollTo({top:0, left:0, behavior:'smooth'})}>TOP</TopBtn>
+    </StListContainer>
     </>
   );
 };
@@ -65,15 +68,15 @@ const StListContainer = styled.div`
   width: 1200px;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items:flex-start;
   margin: 30px auto;
 `;
 const StButtonContainer = styled.div`
   display: flex;
-  justify-content: flex-start;
-  gap: 375px;
-  margin-left: 75px;
-`;
+  justify-content:flex-start;
+  gap:375px;
+  margin-left:75px;
+`
 const StList = styled.div`
   display: flex;
   flex-direction: row;
@@ -85,24 +88,24 @@ const StList = styled.div`
   padding: 30px 0 0 75px;
 `;
 const TopBtn = styled.button`
-  width: 50px;
-  height: 50px;
+  width:50px;
+  height:50px;
+  
+  font-weight:700;
 
-  font-weight: 700;
+  border:none;
+  border-radius:50%;
 
-  border: none;
-  border-radius: 50%;
-
-  background-color: initial;
+  background-color:initial;
   box-shadow: 0px 0px 10px gray;
 
-  margin: 48% 0 0 80%;
-  position: fixed;
+  margin:48% 0 0 80% ;
+  position:fixed;
 
   cursor: pointer;
 
-  transition: all 200ms;
-  :hover {
-    transform: scale(1.2);
-  }
+  transition:all 200ms;
+  :hover{
+    transform:scale(1.2)
+  };
 `;
